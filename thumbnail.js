0369da99318c2a4ba08d406902b9d9e56ece61b3
7c00 <i@7c00.cc>
2017-03-01 18:22:15 +0800

    添加注释

--- thumbnail.js
+++ thumbnail.js
@@ -1,3 +1,6 @@
+/**
+ * 缩略图模块
+ */
 class Thumbnail {
   constructor(canvasContext, scale, $viewer) {
     this.$root = document.querySelector('.thumbnail')
@@ -26,22 +29,41 @@ class Thumbnail {
       this.mousedown = false
     })
     this.$root.addEventListener('mousemove', event => {
+      console.log(event)
       if (this.mousedown) {
         this.updatePosition(event)
       }
     })
   }
 
+  /**
+   * 更新缩略图
+   */
   draw() {
     this.canvasContext.drawImage(
       this.sourceContext.canvas, 0, 0, this.$canvas.width, this.$canvas.height)
   }
 
+  /**
+   * 根据鼠标事件更新指示器位置
+   *
+   * @param event
+   */
   updatePosition(event) {
     const left = event.x - this.$root.offsetLeft - this.indicatorWidth / 2
     const top = event.y - this.$root.offsetTop - this.indicatorHeight / 2
     this.$indicator.style.top = top + 'px'
     this.$indicator.style.left = left + 'px'
+
+    /**
+     * 触发移动事件，调用者可以通过 addEventListener 监听 move 事件
+     * 然后通过 event.detail 得到移动的位置
+     *
+     * @event move
+     * @type {object}
+     * @property {float} x - X 偏移量，取值范围 [0, 1]
+     * @property {float} y - Y 偏移量，取值范围 [0, 1]
+     */
     this.$root.dispatchEvent(new CustomEvent('move', {
       detail: {
         x: left / this.$canvas.width,
@@ -50,6 +72,12 @@ class Thumbnail {
     }))
   }
 
+  /**
+   * 从外部设置指示器位置
+   *
+   * @param {float} offsetX - X 偏移量，取值范围 [0, 1]
+   * @param {float} offsetY - Y 偏移量，取值范围 [0, 1]
+   */
   setPosition(offsetX, offsetY) {
     this.$indicator.style.top = offsetY * this.$canvas.height + 'px'
     this.$indicator.style.left = offsetX * this.$canvas.width + 'px'
